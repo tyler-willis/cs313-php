566d8032fd5636dcce3f5eb01d826d88e8df6dfa
tyler-willis <wil14092@byui.edu>
2018-02-20 20:52:23 -0700

    Added database access

--- web/database/test.php
+++ web/database/test.php
@@ -15,7 +15,7 @@
         try
         {
             // Create the PDO connection
-            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            $db = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
             // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
             $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         }
