f59a1dc7b5a0848ae29820ff6d688ee7528cb682
tyler-willis <wil14092@byui.edu>
2018-02-22 16:49:18 -0700

    Fixed another bug

--- web/database/insert.php
+++ web/database/insert.php
@@ -9,21 +9,6 @@
         $dbHost = 'ec2-54-83-203-198.compute-1.amazonaws.com';
         $dbPort = '5432';
         
-        try
-        {
-            // Create the PDO connection
-            $db = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
-            // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        }
-        catch (PDOException $ex)
-        {
-            // If this were in production, you would not want to echo
-            // the details of the exception.
-            echo "Error connecting to DB. Details: $ex";
-            die();
-        }
-        
         $title = $_GET["title"];
         $console =$_GET["console"];
         $company =$_GET["company"];
@@ -36,8 +21,22 @@
         
         echo $command;
         
-        $statement = $db.prepare($command);
-        $statement->execute();
+        try
+        {
+            // Create the PDO connection
+            $db = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+            // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+            $statement = $db.prepare($command);
+            $statement->execute();
+        }
+        catch (PDOException $ex)
+        {
+            // If this were in production, you would not want to echo
+            // the details of the exception.
+            echo "Error connecting to DB. Details: $ex";
+            die();
+        }
         
         ?>
     </body>
