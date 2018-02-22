ebc39e5f1d3b70057b21bae5163902eff57900b9
tyler-willis <wil14092@byui.edu>
2018-02-22 16:09:27 -0700

    More testing

--- web/database/insert.php
+++ web/database/insert.php
@@ -2,5 +2,17 @@
     <body>
         Name: <?php echo $_GET["name"]; ?><br>
         Console: <?php echo $_GET["console"]; ?>
+        
+        <?php
+        $title = $_GET["title"];
+        $console =$_GET["console"];
+        $company =$_GET["company"];
+        $description =$_GET["description"];
+        $release_date =$_GET["release_date"];
+        $genre =$_GET["genre"];
+        $esrb =$_GET["esrb"];
+        
+        echo $title . $console . $company . $description . $release_date . $genre . $esrb;
+        ?>
     </body>
 </html>
