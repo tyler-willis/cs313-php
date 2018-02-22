2a1c65e70409385a7c9a27bdcb84168a9729febc
tyler-willis <wil14092@byui.edu>
2018-02-22 16:42:14 -0700

    Added some functionality

--- web/database/insert.php
+++ web/database/insert.php
@@ -1,9 +1,29 @@
 <html>
     <body>
-        Name: <?php echo $_GET["name"]; ?><br>
-        Console: <?php echo $_GET["console"]; ?>
         
         <?php
+        
+        $dbUser = 'ucegihpgymiqsr';
+        $dbPassword = '07ed1204e14e2d3103cf2a8260367506829fdba53cf382643e899cebebddaea6';
+        $dbName = 'd2k53fohtc270j';
+        $dbHost = 'ec2-54-83-203-198.compute-1.amazonaws.com';
+        $dbPort = '5432';
+        
+        try
+        {
+            // Create the PDO connection
+            $db = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+            // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        }
+        catch (PDOException $ex)
+        {
+            // If this were in production, you would not want to echo
+            // the details of the exception.
+            echo "Error connecting to DB. Details: $ex";
+            die();
+        }
+        
         $title = $_GET["title"];
         $console =$_GET["console"];
         $company =$_GET["company"];
@@ -12,7 +32,11 @@
         $genre =$_GET["genre"];
         $esrb =$_GET["esrb"];
         
-        echo $title . $console . $company . $description . $release_date . $genre . $esrb;
+        $command = "INSERT INTO game_info (title, description, release_date, genre, esrb, company, console) VALUES ('$title', '$description', '$release_date', '$genre', '$esrb', '$company', '$console')"; 
+        
+        $statement = $db.prepare($command);
+        $statement->execute();
+        
         ?>
     </body>
 </html>
