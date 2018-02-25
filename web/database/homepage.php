77a9c30787955585aa09e11080889176a2eb212e
tyler-willis <wil14092@byui.edu>
2018-02-24 20:19:22 -0700

    Making some minor changes

--- web/database/homepage.php
+++ web/database/homepage.php
@@ -13,9 +13,9 @@
                 <a href="insert.html">Add a game</a>
             </div>
         
-            <p1>
-            Welcome to Video Games Archive! We love<br>all kinds of video games here. Our goal is to create a list<br>of all video games ever played! Visit the archive and add any<br>games you don't see on the list.
-            </p1>
+            <div id="intro">
+            Welcome to Video Games Archive! We love<br>all kinds of video games here. Our goal is to create a list<br>of all video games ever played! Visit the archive and<br>add any games you don't see on the list.
+            </div>
                 
         </div>
     </body>
