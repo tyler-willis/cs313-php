893ff6cc5172a7398a03fd87314fb44bfe5f3efb
tyler-willis <wil14092@byui.edu>
2018-02-24 18:43:53 -0700

    Doing some testing

---
+++ web/database/archive.php
@@ -0,0 +1,44 @@
+<html>
+    <head>
+        <title>Archive</title>
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <body>
+        <div id="container">
+            <header>
+                <h1>VIDEO GAMES<br>ARCHIVE</h1>
+            </header>
+            <div id="subheader">
+                <a href="homepage.php">Back to Home</a>
+                <a href="insert.html">Add a game</a>
+            </div>
+            <div id="games">
+            
+            <?php
+
+            require("dbConnect.php");
+
+            $db = get_db();
+
+            $statement = $db->prepare('SELECT * FROM game JOIN game_info ON game.game_info = game_info.id');
+            $statement->execute();
+        
+            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<div id="game">';
+            
+                echo $row['title'] . "<br>" . $row['company'] . " " . $row['console'] . "<br>";
+            
+                echo $row['genre'] . " - ESRB Rating: " . row['esrb'];
+                
+                echo $row['description'];
+            
+                echo '</div>';
+            }
+
+            ?>
+                
+            </div>
+        </div>
+    </body>
+</html>
--- web/database/homepage.php
+++ web/database/homepage.php
@@ -9,32 +9,11 @@
                 <h1>VIDEO GAMES<br>ARCHIVE</h1>
             </header>
             <div id="subheader">
-                <a href="test.html">View all Video Games</a>
+                <a href="archive.php">View Archive</a>
                 <a href="insert.html">Add a game</a>
             </div>
         
-            <?php
-
-            require("dbConnect.php");
-
-            $db = get_db();
-
-            $statement = $db->prepare('SELECT * FROM game JOIN game_info ON game.game_info = game_info.id');
-            $statement->execute();
-        
-            while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-            {
-                echo '<p>';
             
-                echo "Number of Copies: " . $row['num_copies'] . "<br>" . $row['title'] . "<br>" . $row['description'];
-            
-                echo "<br><br>";
-            
-                echo '</p>';
-            }
-
-            ?>
-            </p>
                 
         </div>
     </body>
